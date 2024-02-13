43587b59bbb43a8de09285878c838b1ecb00acf1
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2024-02-13 12:22:47 -0300

    chore: db instance as scoped instead of transitient

--- Program.cs
+++ Program.cs
@@ -1,12 +1,9 @@
-using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Text.Json.Serialization.Metadata;
 using Npgsql;
 using Rinha2024.Dotnet;
 using Rinha2024.Dotnet.DTOs;
 using Rinha2024.Dotnet.Extensions;
 
-var dbHost = Environment.GetEnvironmentVariable("DB_HOSTNAME");
 var builder = WebApplication.CreateSlimBuilder(args);
 builder.WebHost.UseKestrel(opt =>
 {
@@ -17,7 +14,7 @@ builder.Services.ConfigureHttpJsonOptions(options =>
 {
     options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
 });
-builder.Services.AddTransient<NpgsqlConnection>(_ => new NpgsqlConnection(builder.Configuration.GetConnectionString("DB")));
+builder.Services.AddScoped<NpgsqlConnection>(_ => new NpgsqlConnection(builder.Configuration.GetConnectionString("DB")));
 builder.Services.AddSingleton<ExceptionMiddleware>();
 builder.Services.AddTransient<Service>();
 builder.Services.AddLogging(l => l.AddSimpleConsole());
