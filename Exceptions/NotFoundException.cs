c69f1f99188ae1c27cf2134746a1c3ab9948dd5d
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2024-02-26 01:18:15 -0300

    chore: remove useless code

--- Exceptions/NotFoundException.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Net;
-
-namespace Rinha2024.Dotnet.Exceptions;
-
-public class NotFoundException : Exception
-{
-    public HttpStatusCode StatusCode { get; } = HttpStatusCode.NotFound;
-}
